968477398e5281d7377332ee66c82436414047cf
liyanes <liyanes@outlook.com>
2023-06-04 11:44:07 +0800

    feat: add memory when calling command

--- aicompleter/memory/utils.py
+++ aicompleter/memory/utils.py
@@ -39,3 +39,31 @@ def getMemoryItem(text:str, data:Any, class_:str = 'default'):
     return MemoryItem(vertex=VectexTransformer(st.SentenceTransformer('distilbert-base-nli-mean-tokens')).transform_text(text),
                     class_=class_,
                     data=data)
+
+@attr.s
+class MemoryConfigure:
+    '''
+    Configure of the memory
+    '''
+    factory: type = attr.ib(default=Memory, validator=attr.validators.instance_of(Memory))
+    'The factory of the memory'
+    factory_args: tuple = attr.ib(default=(), validator=attr.validators.instance_of(tuple))
+    'The args of the factory'
+    factory_kwargs: dict = attr.ib(default={}, validator=attr.validators.instance_of(dict))
+    'The kwargs of the factory'
+    vertex_model: Model = attr.ib(default=st.SentenceTransformer(), validator=attr.validators.instance_of(Model))
+    'The vertex model of the memory'
+
+    initial_memory: Optional[Memory] = attr.ib(default=None)
+    'The initial memory of the memory'
+
+    def __attrs_post_init__(self) -> None:
+        if self.initial_memory is None:
+            self.initial_memory = self.factory(*self.factory_args, **self.factory_kwargs)
+    
+    # Check the type of the initial memory
+    @initial_memory.validator
+    def check_initial_memory(self, attribute: str, value: Optional[Memory]) -> None:
+        if value != None and self.factory != value.__class__ and self.factory != None:
+            raise ValueError(f"Initial memory must be {self.factory.__name__}.")
+        self.factory = value.__class__

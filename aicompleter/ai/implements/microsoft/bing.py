0d9edff6c2f0c6f0012ce9c0c9ae868a2f52a725
liyanes <liyanes@outlook.com>
2023-06-19 17:54:58 +0800

    feat: ai support bing linked

--- aicompleter/ai/implements/microsoft/bing.py
+++ aicompleter/ai/implements/microsoft/bing.py
@@ -26,16 +26,28 @@ class BingAI(ai.ChatTransformer):
     def __init__(self, config:Config) -> None:
         super().__init__()
         self._bot_map:dict[uuid.UUID, Chatbot] = {}
-        self._record_map:dict[uuid.UUID, Conversation] = {}
+        self.update_config(config)
+
+    def update_config(self, config:Config) -> None:
         self.proxy:Optional[str] = config.get('proxy', None)
+
+    def new_history(self, user:Optional[str] = None, id:Optional[uuid.UUID] = None) -> Conversation:
+        '''
+        Create a new history
+        '''
+        return Conversation(user=user, id=id or uuid.uuid4(), time=time.time(), timeout=60*60*3, data={'num':0,'continue':True})
     
-    async def ask(self, message: ai.Message, id:uuid.UUID = uuid.uuid4(), style:Style = Style.balanced, search_result:bool = True) -> Coroutine[ai.Message, Any, None]:
+    async def ask(self, message: ai.Message, history: Conversation, style:Style = Style.balanced, search_result:bool = True) -> Coroutine[ai.Message, Any, None]:
         '''
         Ask the AI
         '''
+        id = history.id
         self._bot_map.setdefault(id, Chatbot(proxy=self.proxy))
         bot = self._bot_map[id]
-        self._record_map.setdefault(id, Conversation(timeout=60*60*3,data={'user_message':0}))
+
+        if not history.data['continue']:
+            del self._bot_map[id]
+            raise error.AIGenerateError("Conversation Ended")
         
         async for ret in bot.ask_stream(message.content, style=style.value, search_result=search_result):
             ret_messages = ret['item']['messages']
@@ -44,4 +56,4 @@ class BingAI(ai.ChatTransformer):
                     if i['author'] == 'bot' and 'text' in i:
                         yield i['text']
             messages = list(__bot_messages())
-            yield ai.Message(content=messages[0] if len(messages) == 0 else messages[-1], data=ret,role='assistant')
+            yield ai.Message(content='\n\n'.join(messages), data=ret ,role='assistant')

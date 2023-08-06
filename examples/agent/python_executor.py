c3c833baf27bb67440a8808272c08bb390060e09
liyanes <liyanes@outlook.com>
2023-08-06 17:30:33 +0800

    feat: add examples and new interface.

---
+++ examples/agent/python_executor.py
@@ -0,0 +1,83 @@
+'''
+This is a simple example of how to use the executor interface.
+This will enable the agent to execute python code.
+'''
+
+import aicompleter as ac
+import sys
+import os
+
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../..')))
+
+# Set global log level to DEBUG
+# This is sometimes important because GPT may continously generate wrong commands
+ac.log.setLevel(ac.log.DEBUG)
+
+config = ac.Config({
+    "global": {
+        "openai": {
+            "api-key": "<Input your api key here>",
+            # OpenAI API key (Required)
+            # You can get it from https://platform.openai.com/account/api-keys
+        },
+    },
+    "openaichat": {},
+})
+config.update_global()
+
+# Load the handler
+handler = ac.Handler(config=config)
+# Load AI (OpenAI GPT for chat)
+ai = ac.ai.openai.Chater(config=config['openaichat'])
+
+# Setup the AI to Executor Interface
+# This interface will load an agent to interact with the user
+agent_int = ac.implements.logical.SelfStateExecutor(
+    ai=ai, namespace='openaichat')
+
+# Console interface
+# This interface will get input from console
+# This interface contain a command named 'ask' which will enable the AI to ask the user
+console_int = ac.implements.ConsoleInterface()
+# Commands : ask, echo
+
+# Python Code Interface
+python_int = ac.implements.PythonCodeInterface()
+# Commands : exec
+
+# Author Interface
+# This interface will check the authority of all commands
+# Ask for permission if the authority is not enough
+author_int = ac.implements.AuthorInterface()
+# Provide no commands
+
+
+async def main():
+    # Add graph to control privilege
+    grpah = ac.layer.InterfaceDiGraph()
+    # Enable agent to call the commands of ConsoleInterface (ask, echo)
+    grpah.add(agent_int, console_int)
+    grpah.add(console_int, python_int)
+    grpah.add(python_int, author_int)  # Sometimes it's not necessary
+
+    await grpah.setup(handler)
+
+    # Open a new session
+    session = await handler.new_session()
+
+    # Start conversation
+    # In this example, you can ask the agent: What commands can you execute?
+    init_word = await session.asend(ac.Message(
+        cmd='ask',
+        content={'content': 'Please start a conversation'},
+        dest_interface=console_int,
+    ))
+    # Start agent
+    await session.asend(ac.Message(
+        cmd='agent',
+        content=init_word,
+        dest_interface=agent_int,
+    ))
+
+ac.utils.start(main())
---
+++ examples/interface.py
@@ -0,0 +1,88 @@
+import sys, os
+from typing import Coroutine
+import uuid
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import aicompleter as ac
+
+ac.log.setLevel(ac.log.DEBUG)
+
+class CustomInterface(ac.Interface):
+    '''
+    Make a custom interface
+    '''
+    cmdreg = ac.Commands() # Commands Register, you can add custom command easily by this
+
+    def __init__(self, config: ac.Config = ac.Config(), id:uuid.UUID = uuid.uuid4()):
+        # __init__ method should contain config parameter
+        super().__init__(
+            namespace = 'custom',               # Namespace of the interface, every Interface should have a unique namespace
+
+            user=ac.User(                       # User of the interface
+                                                # Most of time, it just plays a role of permission control
+                                                # And information display
+
+                name = 'custom-user',           # User name
+                description= 'Custom user',
+                in_group = 'system',            # User main group
+                support = {'text'}              # User support
+            )
+        )
+
+    @cmdreg.register('customcmd', 'Just a custom command')
+    async def cmd_customcmd(self, session: ac.Session, message: ac.Message):
+        # Do something
+        self.logger.info("Here, the custom command is called, name: customcmd")
+        self.logger.info("The message is: %s" % message.content)
+        return None # You can return a value 
+    
+    @cmdreg.register('customcmd2', 'Another command',
+                    format={'param1':'value1'})
+    # This command requires a parameter named 'param1' with value description 'value1'
+    # There is a format check on this command, if not satisfied, the command will not be executed and a exception will be raised
+    async def cmd_customcmd2(self, session: ac.Session, message: ac.Message, data: ac.EnhancedDict, config: ac.Config):
+        # command can have two optional parameters
+        # data: session data on this interface, can be get by 'self.getdata(session)'
+        # config: session config on this interface (including this), can be get by 'self.getconfig(session)'
+        self.logger.info("Here, the custom command is called, name: customcmd2")
+        self.logger.info("The message is: %s" % message.content)
+        self.logger.info("The param1 is: %s" % message['param1'])
+        self.logger.info("The data is: %s" % data)
+        return "values"
+    
+    async def init(self, in_handler: ac.Handler) -> Coroutine[None, None, None]:
+        # in_handler is optional
+        # This will be called when the interface is added to the handler
+        await super().init(in_handler)
+
+    async def final(self) -> Coroutine[None, None, None]:
+        # This will be called when the interface is removed from the handler,
+        # or when the handler is closed
+        await super().final()
+
+    async def session_init(self, session: ac.Session):
+        # This is called when a session is created, you can have some operations on the new created session
+        await super().session_init(session)
+
+    async def session_final(self, session: ac.Session):
+        # When a session is closed, the method would be called
+        await super().session_final(session)
+
+@CustomInterface.cmdreg.register('custom3', '...')
+async def cmd_customcmd3(session: ac.Session, message: ac.Message):
+    # You can also register a command outside the interface
+    ac.log.info("Here, the custom command is called, name: customcmd3")
+    ac.log.info("The message is: %s" % message.content)
+    return None
+
+custom_int = CustomInterface()
+handler = ac.Handler()
+
+async def main():
+    await handler.add_interface(custom_int)
+    session = await handler.new_session()
+    await session.asend(ac.Message('customcmd', 'Hello world!'))
+    await session.asend(ac.Message('customcmd2', {'param1':'Hello world!'}))
+    await session.asend(ac.Message('custom3', 'Hello world!'))
+
+ac.utils.start(main())

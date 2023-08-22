e1d36231debd320d846f0bff4d4b8e25e5a97ab4
liyanes <liyanes@outlook.com>
2023-08-22 20:03:50 +0800

    feat: add new agent support.

--- aicompleter/ai/agent/reagent.py
+++ aicompleter/ai/agent/reagent.py
@@ -6,27 +6,25 @@ This is a agent class which put more details to the Interface class.
 import asyncio
 import contextlib
 import copy
-import json
 from typing import Any, Callable, Coroutine, Iterable, Optional, Self
 from ... import log
-from ... import utils
-from ...common import AsyncLifeTimeManager, JsonType
-from ..ai import Conversation, ChatTransformer
+from ...common import AsyncLifeTimeManager
+from ..ai import AuthorType, Conversation, ChatTransformer
 from .. import ai as aiclass
 
 class Agent(AsyncLifeTimeManager):
     '''
     Agent for the chatbot
     '''
-    def __init__(self, ai: ChatTransformer, name:str = 'root', init_conversation: Optional[Conversation] = None, *, loop: Optional[asyncio.AbstractEventLoop] = None):
+    def __init__(self, ai: ChatTransformer, init_conversation: Optional[Conversation] = None, name:str = 'root',  *, loop: Optional[asyncio.AbstractEventLoop] = None):
         super().__init__()
         self._ai = ai
-        self._conversation = init_conversation if init_conversation is not None else Conversation()
+        self.conversation = init_conversation if init_conversation is not None else Conversation()
         if loop == None:
             self._loop = asyncio.get_event_loop()
         else:
             self._loop = loop
-        self._on_response: Callable[[Self,aiclass.Message], Coroutine[None, None, None]] = lambda agent, message: None
+        self._on_response: Callable[[Self,aiclass.Message], Coroutine[None, None, None]] = lambda agent, message: self._loop.create_future()
         '''
         If the AI result is returned, the function will be called
         '''
@@ -37,11 +35,25 @@ class Agent(AsyncLifeTimeManager):
         self._pre_request_time: float = 0.1                                             
         # The time between the requests, for example, the agent got a request at time 0,
         # and the next request(and all new requests) will be sent at time 0.1
-        self._close_tasks.append(loop.create_task(self._agent_loop()))
+        self._close_tasks.append(self._loop.create_task(self._agent_loop()))
 
         self._result = None
 
         self._subagents: dict[str, Self] = {}
+
+    def on_response(self, func: Callable[[Self,aiclass.Message], Coroutine[None, None, None]]):
+        '''
+        Set the on_response function
+        '''
+        self._on_response = func
+
+    @property
+    def config(self):
+        return self._ai.config
+    
+    @config.setter
+    def config(self, value):
+        self._ai.config = value
 
     @property
     def result(self) -> Any:
@@ -65,7 +77,7 @@ class Agent(AsyncLifeTimeManager):
                 return [agent._name]
             else:
                 return [*_get_parents_name(agent._parent), agent._name]
-        agent = cls(parent._ai, name, parent._conversation, loop=parent._loop)
+        agent = cls(parent._ai, parent.conversation, name, loop=parent._loop)
         agent._parent = parent
         agent._logger.pop()
         agent._logger.push(_get_parents_name(agent))
@@ -87,18 +99,22 @@ class Agent(AsyncLifeTimeManager):
                 requests.append(request)
                 await asyncio.sleep(self._pre_request_time)
                 while not self._request_queue.empty():
-                    requests.append(self._request_queue.get_nowait())
-            
+                    to_add = self._request_queue.get_nowait()
+                    if to_add != None:
+                        requests.append(to_add)
+            for request in requests:
+                self._logger.debug('Got request: %s', repr(request))
+
             @contextlib.contextmanager
             def _append_conversation():
                 # Append the conversation to the conversation list
                 # The conversation will be removed when the context is exited
-                before = copy.copy(self._conversation)
-                self._conversation.messages.extend(requests)
+                before = copy.copy(self.conversation)
+                self.conversation.messages.extend(requests)
                 try:
-                    yield self._conversation
+                    yield self.conversation
                 except Exception as e:
-                    self._conversation = before
+                    self.conversation = before
                     raise e
                 
             with _append_conversation() as conversation:
@@ -106,18 +122,26 @@ class Agent(AsyncLifeTimeManager):
                     reader: asyncio.StreamReader = asyncio.StreamReader()
                     logtask = self._loop.create_task(self._logger.debug_stream(reader))
                     last_content = ''
-                    async for message in self._ai.generate(conversation):
-                        new_content = message.content[len(last_content):]
-                        last_content = message.content
-                        if new_content:
-                            reader.feed_data(new_content.encode())
+                    reader.feed_data('AI response: '.encode())
+                    try:
+                        async for message in self._ai.generate(conversation):
+                            new_content = message.content[len(last_content):]
+                            last_content = message.content
+                            if new_content:
+                                reader.feed_data(new_content.encode())
+                    except Exception as e:
+                        reader.feed_eof()
+                        await logtask
+                        self._logger.exception('Error when generating message: %s', e)
+                        raise e
+                    reader.feed_eof()
                     await logtask
                     del reader, last_content, new_content, logtask
                 else:
                     message = await self._ai.generate_message(conversation)
                 # The message is generated
             
-            self._conversation.messages.append(message)
+            self.conversation.messages.append(message)
             # parse the message
             await self._on_response(self, message)
 
@@ -128,6 +152,30 @@ class Agent(AsyncLifeTimeManager):
         if self.closed:
             raise RuntimeError('The agent is closed')
         self._result = result
+
+    def append_message(self, message: aiclass.Message):
+        '''
+        Append a message to the conversation
+        '''
+        if self.closed:
+            raise RuntimeError('The agent is closed')
+        self._request_queue.put_nowait(message)
+
+    def append_word(self, word: str, user:Optional[str] = None, data:Optional[dict] = None):
+        '''
+        Append a user message to the conversation
+        '''
+        if self.closed:
+            raise RuntimeError('The agent is closed')
+        self._request_queue.put_nowait(aiclass.Message(word, role=AuthorType.USER, user=user, data=data))
+
+    def trigger(self):
+        '''
+        Trigger the agent, without any message
+        '''
+        if self.closed:
+            raise RuntimeError('The agent is closed')
+        self._request_queue.put_nowait(None)
 
     def __await__(self):
         yield from self._close_event.wait().__await__()
